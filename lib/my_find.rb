bef5c0b523a731fdcbbd627914dac610f350f6f3
Clint Richards <richardsclint17@gmail.com>
2020-11-29 23:12:20 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,11 +1,11 @@
-
-  while i < collection.length	  while i < collection.length
-    if yield(collection[i])	    return collection[i] if yield(collection[i])
-      block_return_values << collection[i]
-    end
-    i = i + 1	    i = i + 1
-  end	  endrequire 'pry'
+require 'pry'
 
 def my_find(collection)
-
+  
+    while i < collection.length	  while i < collection.length
+      if yield(collection[i])	    return collection[i] if yield(collection[i])
+        block_return_values << collection[i]	
+      end	
+      i = i + 1	    i = i + 1
+    end
 end
