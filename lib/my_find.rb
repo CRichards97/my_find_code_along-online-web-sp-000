a7eefe560c6ac2cb0a4f7571cdfa946178fe55c7
Clint Richards <richardsclint17@gmail.com>
2020-11-29 23:12:12 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,8 +1,8 @@
 
   while i < collection.length	  while i < collection.length
     if yield(collection[i])	    return collection[i] if yield(collection[i])
-      block_return_values << collection[i]	
-    end	
+      block_return_values << collection[i]
+    end
     i = i + 1	    i = i + 1
   end	  endrequire 'pry'
 
